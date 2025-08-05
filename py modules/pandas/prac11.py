12d801030ac51f55c8f75992629c142ebf15d2d3
Shubham <149764503+shubhxm15@users.noreply.github.com>
2025-08-06 00:18:00 +0530

    Update prac11.py

--- py modules/pandas/prac11.py
+++ py modules/pandas/prac11.py
@@ -3,21 +3,26 @@ import pandas as pd
 data1 = {'Id': ['E01', 'E02', 'E03', 'E04', 'E05', 'E06'],
          'Name': ['Tapu', 'Gogi', 'Goli', 'Pinku', 'Sonu', 'Champaklal']}
 
-data2 = {'Id': ['E01', 'E02', 'E03', 'E04', 'E05', 'E06'],
+data2 = {'Id': ['E01', 'E02', 'E03', 'E04', 'E05', 'E013'],       # will show nan value in salary
          'Salary': [40000,50000,60000,70000,80000,90000]}
 
 a = pd.DataFrame(data1)
 b = pd.DataFrame(data2)
 
-c = pd.merge(a,b, on = 'Id')
+c = pd.merge(a,b, on = 'Id', how = 'left')
 print(c)
 
 data3 = {'Id': ['E07', 'E08', 'E09', 'E10', 'E11', 'E12'],
-         'Name': ['Kshitij', 'Krish', 'Aayush', 'Japjot', 'Chetanya', 'Shubham']}
+         'Name': ['Kshitij', 'Krish', 'Aayush', 'Japjot', 'Chetanya', 'Shubham'],
+         'Salary': [2000,3000,5000,10000,50000,90000]}
 
 data4 = {'Id': ['E07', 'E08', 'E09', 'E10', 'E11', 'E12'],
+         'Name': ['Akshaansh', 'Maanit', 'Yaksh', 'Dev', 'Ram', 'Shyam'],
          'Salary': [20000,30000,40000,50000,60000,70000]}
 
-d = pd.merge(data3, data4, on = 'Id')
+d = pd.DataFrame(data3) 
+e = pd.DataFrame(data4)
 
-print(pd.concat([c,d]))
+# f = pd.merge(data3, data4, on = 'Id')
+
+print(pd.concat([d,e]))
